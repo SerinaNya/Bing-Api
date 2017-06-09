ac0e4796cd0909a0a205643a759f36aa0dad8213
Flyfish233 <ffgh2010@live.cn>
2017-06-09 22:15:46 +0800

    Create api.php

--- api.php
+++ api.php
@@ -1,6 +1,8 @@
 <?php
-function 0() {
-    $data = json_decode(file_get_contents('http://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1'), true);
+//设定时间(几天前，整数，0为今天)
+$ago = '0';
+function 0() {include 'settings.php';
+    $data = json_decode(file_get_contents('http://bing.com/HPImageArchive.aspx?format=js&idx=$ago&n=1'), true);
     return "https://cn.bing.com".$data['images'][0]['url'];
 };
 $url = 0();
