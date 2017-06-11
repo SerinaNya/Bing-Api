e9a1c92906c55d266a2146c257acd5873f570aa9
Flyfish233 <ffgh2010@live.cn>
2017-06-11 09:19:57 +0800

    Create api-for-wallpaper-engine.php

--- api-for-wallpaper-engine.php
+++ api-for-wallpaper-engine.php
@@ -3,6 +3,7 @@
 	小垃圾必应日图 API 精简版(LLBDPA) Lite Laji(means rubbish) Bing Daily Picture API(LLBDPA)
 	版本 1.1 (第三方) Version: 1.1 (Third Party)
 	源码 Little_Qiu,感谢 GPlane,精简 Flyfish. Source By Little_Qiu,Thanks @GPlane,Changed By Flyfish.
+	国际版会有延时。会在早晨刷新而不是0:00.
 */
 function bg(){
     //非专业用户，请不要更改以下文本 if U aren't a PHP developer,please don't change the following text.
