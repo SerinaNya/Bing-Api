fbdda2ad03d6c61fae0ddb8e8ef57453d3c9a3b5
LittleStudio-Little_Qiu <littleqzh@gmail.com>
2017-07-09 21:12:41 +0800

    在代码行数上做了一点微小的工作
    精简了三行代码，现在稳定版仅有 43 行代码了。

--- bingpic-test.php
+++ bingpic-test.php
@@ -1,7 +1,7 @@
 <?php
  /* 
 	辣鸡 Bing 今日美图 API 测试版 By Little_Qiu
-	Version: 1.3-test
+	Version: 1.5-test
 	感谢 @GPlane 和 @Flyfish233
 	这是辣鸡 Bing 今日美图 API 的测试版。
     该版本可能未开发完全或含有不稳定因素，请谨慎用于生产环境！
@@ -16,25 +16,22 @@ error_reporting(E_ALL | E_STRICT);
 // 获取 Bing 今日美图的图片地址
 function bg() {
 	// 测试版设置配置文件能否通用
-	$mixSettingsPhp = false;
+	$mixSettingsPhp = true;
 	// 测试版检查配置文件能否通用
-	if (mixSettingsPhp) {
+	if (mixSettingsPhp) { 
 		// 导入设置
-		include 'settings.php'
-	} else {
+		include 'settings.php';
+	} else { 
 		// 导入设置
-		include 'settings-test.php'
-	}
-	// 检查是否使用 URL 指定时间
-	if (useUrlSetDate) {
-		// 获取 daysago 参数的值
-        $daysAgoQuery = "$_GET[daysago]";
-		// 获取 JSON
-        $data = json_decode(file_get_contents("http://bing.com/HPImageArchive.aspx?format=js&idx=$daysAgoQuery&n=1"), true);
-	} else {
-		// 获取 JSON
-        $data = json_decode(file_get_contents("http://bing.com/HPImageArchive.aspx?format=js&idx=$daysAgo&n=1"), true);
+		include 'settings-test.php';
 	};
+	// 检查是否使用 URL 指定时间
+	if (useUrl) {
+		// 将日期设置为 daysago 参数的值
+		$daysAgo = "$_GET[daysago]";
+	};
+	// 获取 JSON
+    $data = json_decode(file_get_contents("http://bing.com/HPImageArchive.aspx?format=js&idx=$daysAgo&n=1"), true);
 	// 返回 URL
     return "https://bing.com".$data['images'][0]['url'];
 };
@@ -42,4 +39,5 @@ function bg() {
 $url = bg();
 header("Location: $url");
 exit;
+//echo $url;
 ?>
--- bingpic.php
+++ bingpic.php
@@ -2,7 +2,7 @@
  
 /* 
 	辣鸡 Bing 今日美图 API By Little_Qiu
-	Version: 1.3
+	Version: 1.5
 	感谢 @GPlane 和 @Flyfish233
 */
  
@@ -17,13 +17,10 @@ function bg() {
 	// 检查是否使用 URL 指定时间
 	if (useUrl) {
 		// 获取 daysago 参数的值
-        $daysAgoQuery = "$_GET[daysago]";
-		// 获取 JSON
-        $data = json_decode(file_get_contents("http://bing.com/HPImageArchive.aspx?format=js&idx=$daysAgoQuery&n=1"), true);
-	} else {
-		// 获取 JSON
-        $data = json_decode(file_get_contents("http://bing.com/HPImageArchive.aspx?format=js&idx=$daysAgo&n=1"), true);
+        $daysAgo = "$_GET[daysago]";
 	};
+	// 获取 JSON
+    $data = json_decode(file_get_contents("http://bing.com/HPImageArchive.aspx?format=js&idx=$daysAgo&n=1"), true);
 	// 返回 URL
     return "https://bing.com".$data['images'][0]['url'];
 };
