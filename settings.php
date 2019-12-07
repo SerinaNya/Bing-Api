254b36c57fc24cf14deccc23768cafa2d91a1dea
Xiao_Jin <34389622+jinzhijie@users.noreply.github.com>
2019-12-07 18:18:28 +0800

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,13 +1,13 @@
 <?php
 /*
-    辣鸡 Bing 今日美图 API 配置文件 By Little_Qiu
-    You can follow annotations edit this file.
+    辣鸡 Bing 今日美图 API 配置文件 By Little_Qiu & Xiao_Jin
+    你可以根据每个配置项上方的注释，修改你的配置文件。
 */
 
-// Use daysago Query to set time? If false, you can set time in next setting.
-$useUrl = true;
-// Set time. Just work when $useUrl is false. 1 is yesterday and 2 is the day before yesterday, -1 is tomorrow...
-$daysAgo = '0';
-// Set update source. You can check update source in: https://github.com/Minecraft-LittleQiu/laji-bing-pic-api/wiki/%E5%A6%82%E4%BD%95%E6%90%AD%E5%BB%BA%E6%9B%B4%E6%96%B0%E6%BA%90-&-%E6%9B%B4%E6%96%B0%E6%BA%90%E5%88%97%E8%A1%A8
+// 是否使用在 URL 后添加 daysago 参数的方法指定时间，若关闭则可在下一条设置项中设置时间
+$useUrl = false;
+// 设置时间（几天前），将 0 修改为你需要的时间，1 为昨天，2 为前天，-1 为明天，以此类推
+$daysAgo = '-1';
+// Set update source. You can check update source in: https://github.com/jinzhijie/LjBingPictureApi/wiki/%E5%A6%82%E4%BD%95%E6%90%AD%E5%BB%BA%E6%9B%B4%E6%96%B0%E6%BA%90-&-%E6%9B%B4%E6%96%B0%E6%BA%90%E5%88%97%E8%A1%A8
 $updateRepo = 'https://pastebin.com/raw/mrvb7Pqw';
 ?>
