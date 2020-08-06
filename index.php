bd52bf9441bb2237ac2a0687eb08899361418b34
Xiao_Jin <34389622+jinzhijie@users.noreply.github.com>
2020-08-06 16:31:16 +0800

    复用部分代码

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
  
 /* 
-	辣鸡 Bing 今日美图 API By Little_Qiu
-	Version: 1.6
-	感谢 @GPlane 和 @Flyfish233
+	repo: https://github.com/jinzhijie/Bing-Api
 */
  
 // 检查 PHP 版本
 if (version_compare(PHP_VERSION, '5.2.0', '<')) {
-    exit('错误：辣鸡 Bing 今日美图 API 要求 PHP 版本不低于 5.2.0，你正在使用的是 '.PHP_VERSION);
+    exit('PHP 版本不得低于 5.2.0，可你正在使用的是 '.PHP_VERSION);
 }
 // 获取 Bing 今日美图的图片地址
 function bg() {
 	// 导入设置
 	include 'settings.php';
+	
 	// 检查是否使用 URL 指定时间
 	if ($useUrl) {
-		// 获取 daysago 参数的值
-        $daysAgoQuery = "$_GET[daysago]";
-		// 获取 JSON
-        $data = json_decode(file_get_contents("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=$daysAgoQuery&n=1"), true);
+        $daysAgoQuery = "$_GET["daysago"]";
+	$data = req($daysAgoQuery);
 	} else {
-		// 获取 JSON
-        $data = json_decode(file_get_contents("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=$daysAgo&n=1"), true);
+        $data = req($daysAgo);
 	};
 	// 返回 URL
     return "https://cn.bing.com".$data['images'][0]['url'];
 };
-// 302 跳转
+
+function req($daysAgo) {
+	retrun json_decode(file_get_contents("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=$daysAgo&n=1"), true);
+};
+
 $url = bg();
 header("status: 302");
 header("Location: $url");
